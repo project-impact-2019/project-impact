5d74b8217ae157fae6387626e5f8de4a55ed81d1
polynova <jocelyncasanova1@gmail.com>
2019-07-30 03:42:35 -0400

    added js to html and imports to views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from core.models import User, Forum, Comment, Category, Resource, BlogPost, ProgressTracker, VisionBoard
+import json
 # Create your views here.
+

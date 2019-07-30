d52dd1a8077e85b133090a68c7f72cab7359fc9e
Devin Muhs <devinmuhs@gmail.com>
2019-07-29 20:15:14 -0400

    added blog and give back pages

--- project_impact/settings.py
+++ project_impact/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -68,7 +69,9 @@ ROOT_URLCONF = 'project_impact.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

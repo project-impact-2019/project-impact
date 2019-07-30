013d42ce5be5f065e3ee464f0a07e5aa488c567f
Michael <phamm89@gmail.com>
2019-07-30 14:36:05 -0400

    Database

--- project_impact/settings.py
+++ project_impact/settings.py
@@ -151,4 +151,4 @@ LOGIN_REDIRECT_URL = '/'
 import django_heroku
 django_heroku.settings(locals())
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)

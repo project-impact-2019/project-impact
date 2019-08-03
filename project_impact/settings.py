40505f62db52b822848f5320d2b1035f6d4de396
Michael <phamm89@gmail.com>
2019-08-03 16:48:13 -0400

    Changed Static

--- project_impact/settings.py
+++ project_impact/settings.py
@@ -144,9 +144,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'static'),)
 
 # Registration
 

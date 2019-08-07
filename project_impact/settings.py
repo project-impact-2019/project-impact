67013ea83195dfe8aef9d9c743a0106a17f6056a
Michael <phamm89@gmail.com>
2019-08-07 16:15:12 -0400

    Installed Bulma

--- project_impact/settings.py
+++ project_impact/settings.py
@@ -56,6 +56,9 @@ INSTALLED_APPS = [
     #crispy-forms
     'crispy_forms',
 
+    # bulma
+    'bulma',
+
 ]
 
 # Custom User Authentication
@@ -148,9 +151,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 
 # Registration
 
